abf32eb6b6ecfa8342f9535e5e73b67f451ed751
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2021-09-17 01:27:35 +0530

    Time: 196 ms (69.43%), Space: 84.3 MB (37.12%) - LeetHub

--- online-stock-span/online-stock-span.cpp
+++ online-stock-span/online-stock-span.cpp
@@ -1,28 +1,20 @@
 class StockSpanner {
 public:
     stack<pair<int,int>> s;
-    vector<int> v;
     int i;
     StockSpanner() {
-        i=-1;
+        i=0;
     }
     
-    int next(int price) {
-        if(!s.size()){
-            v.push_back(-1);
-        }else{
-            while(s.size()>0 && s.top().first<=price){
-                s.pop();
-            }
-            if(!s.size()){
-               v.push_back(-1);
-            }
-            else{
-               v.push_back(s.top().second);
-            }
+    int next(int p) {
+        int count=0;           
+        while(s.size()&&s.top().first<=p){ 
+            count++;    
+            count+=s.top().second;     
+            s.pop();
         }
-        s.push({price,++i});
-        return i-v.back();
+       s.push({p,count});
+        return count+1;
     }
 };
 
