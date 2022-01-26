627d38a5c770d1b518a4a83e7cc42220e5f4d269
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-01-26 16:34:00 +0530

    Delete ARRAYS directory

--- ARRAYS/Series.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Series{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-    }
-} 
