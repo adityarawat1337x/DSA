1a3a6ef83d7fb4e1592d85c78027aa71651f4ed9
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-01-29 00:42:05 +0530

    Time: 4 ms (45.85%), Space: 8.5 MB (22.91%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int l=0,m=0,r=nums.size()-1;
+      
         while(m<=r){
             if(nums[m]==1)
                 m++;
