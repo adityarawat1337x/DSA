8f16af81dc42e9a7a3d4657e6fc49c4d2c9d3801
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-01-29 19:15:45 +0530

    Time: 6 ms (20.38%), Space: 9.1 MB (28.96%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    void arrange(vector<int>& arr){
-        for(int i=0;i<arr.size()-1;i++){
-            if(arr[i]>arr[i+1])
-                swap(arr[i],arr[i+1]);
-        }
-    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int n1=0,n2=0;
-        if(n==0)
-            return;
-        while(n1<m){
-            if(nums1[n1]>nums2[n2]){
-                swap(nums1[n1],nums2[n2]);
-                arrange(nums2);
-            }
-            n1++;
+        int k=nums1.size()-1;
+        int i=m-1;
+        int j=n-1;
+        while(i>=0 && j>=0)
+        {
+            nums1[k--]=(nums1[i]>nums2[j])?nums1[i--]:nums2[j--];
         }
-        
-        while(n1<m+n){
-            nums1[n1]=nums2[n2];
-            n1++;
-            n2++;
+        while(i>=0)
+        {
+            nums1[k--]=nums1[i--];
+        }
+        while(j>=0)
+        {
+            nums1[k--]=nums2[j--];
         }
     }
 };
