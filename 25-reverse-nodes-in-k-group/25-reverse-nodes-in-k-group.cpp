14186ad2214d20fe3352d2ca6c32144551e5d94d
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-01 23:16:09 +0530

    Time: 28 ms (24.23%), Space: 11.5 MB (72.24%) - LeetHub

---
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ **/
+
+class Solution {
+public:
+     pair<ListNode*,ListNode*> rev(ListNode* head,int k) {
+        
+        ListNode* newHead=NULL,*newTail=head;
+        ListNode* tail=head;
+        int i=0;
+        while(tail and i++<k){
+            ListNode* tmp = tail;
+            tail=tail->next;
+            tmp->next=newHead;
+            newHead=tmp;
+        }
+        cout<<"reverse "<<newHead->val<<" "<<newTail->val<<"\n";
+        return {newHead,newTail};
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head)
+            return NULL;
+        ListNode *newHead,*newTail,*tmp=head;
+        int i=0;
+        while(tmp and i<k){
+            tmp=tmp->next;
+            i++;
+        }
+        if(!tmp and i<k)
+            return head;
+        ListNode* ahead = reverseKGroup(tmp,k);
+        
+        auto x = rev(head,k); 
+        newHead=x.first;
+        newTail=x.second;
+        newTail->next=ahead;
+        return newHead;
+    }
+};
