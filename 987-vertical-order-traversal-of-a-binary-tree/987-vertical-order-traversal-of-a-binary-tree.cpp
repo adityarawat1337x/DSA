485c735604019d0c1592dc23c589c37df4fe7f31
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-11 23:24:38 +0530

    Time: 7 ms (55.79%), Space: 14.2 MB (24.65%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,52 +11,39 @@
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-      vector<vector<int>>  ans;
-        queue<pair<TreeNode*,int>> q;
-        int lvl=-1;
-        map<pair<int,int>,vector<int>> mp;
-        q.push({nullptr,-1});
-        q.push({root,(1000/2)});
-        while(q.size()>1 or q.front().first!=nullptr){
-            auto x = q.front();
-            q.pop();
-            auto rt = x.first;
-            auto idx = x.second;
-            
-            if(rt==nullptr){
-                lvl++;
-                q.push({nullptr,-1});
-                auto y = q.front();
-                q.pop();
-                rt = y.first;
-                idx = y.second;
-            }
-            if(mp.find({idx,lvl})!=mp.end()){
-                mp[{idx,lvl}].push_back(rt->val);
-            }else{
-                mp[{idx,lvl}]={rt->val};
-            }
-             if(rt->left)
-                q.push({rt->left,idx-1});
-        
-            if(rt->right)
-                q.push({rt->right,idx+1});
-           
-        }
-        for(int i=0;i<=1000;i++){
-            vector<int> subans;
-            for(int j=0;j<=1000;j++)
-                if(mp.find({i,j})!=mp.end()){
-                    auto tmp = mp[{i,j}];
-                   
-                    if(tmp.size()>1);
-                        sort(tmp.begin(),tmp.end());
-                    subans.insert(subans.end(), tmp.begin(), tmp.end());
-                }
-             if(subans.size())
-                ans.push_back(subans);
-        }
-        return ans;
+   void votHelper(TreeNode* root, std::unordered_map<int, multiset<pair<int, int>>>& dataMap, int row, int col, int& minCol, int& maxCol)
+{
+	if(!root) return;
+	dataMap[col].insert(make_pair(row, root->val));
+	minCol = min(minCol, col);
+	maxCol = max(maxCol, col);
+	votHelper(root->left, dataMap, row+1, col-1, minCol, maxCol);
+	votHelper(root->right, dataMap, row+1, col+1, minCol, maxCol);
+}
+
+vector<vector<int>> verticalTraversal(TreeNode* root)
+{
+	if(!root) return {{}};
+	std::unordered_map<int, multiset<pair<int, int>>> dataMap;
+	// Map for column numbers
+	// multiset to store data in ordered fashion on the basis of <row, values>.
+
+	int minCol=0, maxCol=0;
+	votHelper(root, dataMap, 0, 0, minCol, maxCol);
+	
+	// for(auto itr : dataMap)
+	// 	for(auto itr2 : itr.second)
+	// 		std::cout << itr.first << " " << itr2.first << " " << itr2.second << std::endl;
+	// std::cout << minCol << " " << maxCol;
+	
+	std::vector<vector<int>> votOrder;
+	for(int i = minCol; i<=maxCol; i++)
+    {
+        std::vector<int> temp;
+		for(auto itr : dataMap[i])
+            temp.push_back(itr.second);
+        votOrder.push_back(temp);
     }
+	return votOrder;
+}
 };
