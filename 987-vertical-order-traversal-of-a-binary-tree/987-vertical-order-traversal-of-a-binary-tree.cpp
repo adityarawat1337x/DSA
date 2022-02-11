13d3831018c4d98f37dc37c250200216e3704d59
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-11 23:32:50 +0530

    Time: 11 ms (25.71%), Space: 12.1 MB (82.47%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,6 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+void votHelper(TreeNode* root, std::unordered_map<int, multiset<pair<int, int>>>& dataMap, int row, int col, int& minCol, int& maxCol)
+{
+	if(!root) return;
+	dataMap[col].insert(make_pair(row, root->val));
+	minCol = min(minCol, col);
+	maxCol = max(maxCol, col);
+	votHelper(root->left, dataMap, row+1, col-1, minCol, maxCol);
+	votHelper(root->right, dataMap, row+1, col+1, minCol, maxCol);
+}
+
+vector<vector<int>> verticalTraversal(TreeNode* root)
+{
+	if(!root) return {{}};
+	std::unordered_map<int, multiset<pair<int, int>>> dataMap;
+	int minCol=0, maxCol=0;
+	votHelper(root, dataMap, 0, 0, minCol, maxCol);
+	
+	std::vector<vector<int>> votOrder;
+	for(int i = minCol; i<=maxCol; i++)
+    {
+        std::vector<int> temp;
+		for(auto itr : dataMap[i])
+            temp.push_back(itr.second);
+        votOrder.push_back(temp);
+    }
+	return votOrder;
+}
+
+*/
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
