ee0fa920a9122a0a9d12da66e743fbb0c74aec38
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-12 00:54:18 +0530

    Time: 15 ms (38.26%), Space: 17.2 MB (94.90%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,51 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        long ans=0,subans=0;
-        if(!root)
-            return ans;
-        queue<pair<TreeNode*,long>> q;
-        q.push({NULL,-1});
-        q.push({root,0});
-        long last=0;
-        while(q.size()>1){
-            auto x = q.front();
-            q.pop();
-            
-            if(!x.first){
-                x = q.front();
-                subans = x.second;
-                last = x.second;
+        if(!root->left && !root->right) return 1;
+        long ans = INT_MIN, level; 
+        queue<pair<TreeNode *, long> > q;
+        q.push({root, 0}); 
+        while(!q.empty()){
+            long s = q.size(); 
+            long mmin = q.front().second, f, l;            
+            for(long i = 0; i < s; i++) {
+                // Dequeue element
+                TreeNode *currNode = q.front().first;
+                long curID = q.front().second - mmin;
+                
                 q.pop();
-                q.push({nullptr,-1});
+                
+                if(i == 0) f = curID;
+                if(i == s - 1) l = curID;
+                if (currNode->left){
+                    q.push({currNode->left, curID*2+1});
+                }
+                
+                if (currNode->right){
+                    q.push({currNode->right, curID*2+2});
+                }
             }
-            
-            auto rt = x.first;
-            auto idx = x.second;
-            auto idx2 = x.second-last;
-            
-            if(rt->left){
-                q.push({rt->left,(long)2*(long)idx2});
-            }
-            
-            if(rt->right){
-                q.push({rt->right,(long)(2*idx2+1)});
-            }
-            
-            if(q.front().first==nullptr){
-                ans = max(ans,idx-subans+1);
-            }
+            ans = max(ans , l-f+1);
         }
         
         return ans;
