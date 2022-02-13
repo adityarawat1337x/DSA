8576320e45bc07f058d97287e69e480c38856654
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-13 17:33:25 +0530

    Time: 24 ms (67.63%), Space: 26.4 MB (45.03%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,18 +10,22 @@
  * };
  */
 class Solution {
+    unordered_map<int,int> mp;
 public:
       TreeNode* help(vector<int>& preorder, vector<int>& inorder,int l,int r) {
         if(l>r)
             return nullptr;
         TreeNode* root =new TreeNode(preorder[0]);
         preorder.erase(preorder.begin());
-        auto it = find(inorder.begin(),inorder.end(),root->val);
-        root->left = help(preorder,inorder,l,it-inorder.begin()-1);
-        root->right = help(preorder,inorder,it-inorder.begin()+1,r);
+        auto it = mp[root->val];
+        root->left = help(preorder,inorder,l,it-1);
+        root->right = help(preorder,inorder,it+1,r);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for(int i=0;i<inorder.size();i++){
+            mp[inorder[i]]=i;
+        }
         return help(preorder,inorder,0,preorder.size()-1);
     }
 };
