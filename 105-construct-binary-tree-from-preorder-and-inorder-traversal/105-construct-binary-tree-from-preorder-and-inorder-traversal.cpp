36c0dcdfc2008cca6d2712e4088412357934f1c1
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-13 17:30:29 +0530

    Time: 35 ms (45.70%), Space: 26.1 MB (62.38%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,25 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int j=0;
-    unordered_map<int,int> um;
-    TreeNode *fun(vector<int>& pre, vector<int>& in,int s,int e) {
-        if(s>e)
+      TreeNode* help(vector<int>& preorder, vector<int>& inorder,int l,int r) {
+        if(l>r)
             return nullptr;
-        int m = um[pre[j]];
-        TreeNode *root = new TreeNode(pre[j++]);
-        root->left = fun(pre,in,s,m-1);
-        root->right = fun(pre,in,m+1,e);
-        
-
+        TreeNode* root =new TreeNode(preorder[0]);
+        preorder.erase(preorder.begin());
+        auto it = find(inorder.begin(),inorder.end(),root->val);
+        root->left = help(preorder,inorder,l,it-inorder.begin()-1);
+        root->right = help(preorder,inorder,it-inorder.begin()+1,r);
         return root;
-
     }
-
-    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        int n = pre.size();
-        for(int i=0;i<n;i++)
-            um[in[i]]=i;
-        TreeNode *root  = fun(pre,in,0,n-1);
-        return root;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return help(preorder,inorder,0,preorder.size()-1);
     }
 };
