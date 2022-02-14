2c933fc2513a203ec8e728dc6c70ffd177f2e84c
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-14 18:27:19 +0530

    Time: 244 ms (99.90%), Space: 117.1 MB (21.32%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,32 +1,55 @@
 class MedianFinder {
-    priority_queue<int> left;
-    priority_queue<int,vector<int>,greater<int>> right;
-public:
+ public: 
     MedianFinder() {
-        
-    }
-    
-    void addNum(int num) {
-        left.push(num);
-        right.push(left.top());
-        left.pop();
-        if(left.size()<right.size()){
-            auto x = right.top();
-            right.pop();
-            left.push(x);
-        }
-    }
-    
-    double findMedian() {
-        if(left.size()==right.size())
-            return (double)(left.top()+right.top())/2.0;
-        return left.top();
-    }
-};
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
+        size = 0;
+    }
+    ~MedianFinder() = default;
+
+    // O(lg n)
+    // maintain the invariant:
+    // abs(minHeap.size() - maxHeap.size()) <= 1
+    void addNum(int num) {
+        if (size == 0) {
+            minHeap.push(num);
+        } else {
+            if (minHeap.top() > num) {
+                maxHeap.push(num);
+            } else {
+                minHeap.push(num);
+            }
+        }
+
+        int minHeap_size = minHeap.size();
+        int maxHeap_size = maxHeap.size();
+        int sizeDiff = abs(minHeap_size - maxHeap_size);
+        if (sizeDiff > 1) {
+            // move one element from larger to smaller
+            if (minHeap.size() > maxHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            } else {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            }
+        }
+        size++;
+    }
+
+    // O(1)
+    double findMedian() {
+        if (size % 2 == 0) {
+            return (minHeap.top() + maxHeap.top()) / 2.0;
+        } 
+
+        return minHeap.size() > maxHeap.size()? 
+                minHeap.top() : maxHeap.top();
+    }
+
+ private:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int size;
+};
