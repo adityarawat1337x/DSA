2be5ca495a06822b0cf321becffa8328b40ff249
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-21 18:33:31 +0530

    Time: 195 ms (61.54%), Space: 40.6 MB (53.85%) - LeetHub

---
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        vector<long long> ans;
+        if(finalSum&1)
+            return ans;
+        
+        for(int i=2;i<=finalSum;i+=2){
+            ans.push_back(i);
+            finalSum-=i;
+        }
+        
+        ans[ans.size()-1]+=finalSum;
+        
+        return ans;
+    }
+};
