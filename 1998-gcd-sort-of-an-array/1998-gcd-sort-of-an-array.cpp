3cb371b0c82ba4f51c4de5cfdd08eb164514316e
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-23 22:13:12 +0530

    Time: 813 ms (38.50%), Space: 286 MB (5.34%) - LeetHub

--- 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.cpp
+++ 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.cpp
@@ -1,49 +1,51 @@
 class Solution {
-    vector<int> rank,parent;
 public:
-     void un(int u,int v){
-        u = find(u);
-        v = find(v);
-        if(u==v)
-            return;
-        if(rank[u]>rank[v]){
-            parent[v]=u;
-            rank[u]++;
-        }else{
-            parent[u]=v;
-            rank[v]++;
+    vector<int>parent;
+    vector<int>size;
+    int findPar(int x){
+        if(parent[x]==x){
+            return parent[x];
         }
+        return parent[x]=findPar(parent[x]);
     }
-    
-    int find(int child){
-        if(child==parent[child])
-            return child;
-        return parent[child] = find(parent[child]);
-    }
-     
-    
-    bool gcdSort(vector<int>& nums) {
-        int n = *max_element(nums.begin(), nums.end());
-        parent.resize(n+1); rank.resize(n+1);
-        for(int i=0;i<=n;i++){
-            rank[i]=0;
-            parent[i]=i;
+    void unionn(int x,int y){
+        x=findPar(x);
+        y=findPar(y);
+        if(size[x]<=size[y]){
+            parent[x]=y;
+            size[y]++;
         }
+        else if(size[y]<size[x]){parent[y]=x;size[x]++;}
         
-       for (auto u:nums)
-            for (int v = 2; v*v <= u; v ++) 
-                if(u%v==0){
-                    un(u/v,u);
-                    un(u,v);
+    }
+	//n=100000
+    void Sieve(int n,vector<int>& nums,vector<int>&mp){
+        vector<bool>prime(n,true);
+        for(int i=2;i<=n;i++){
+            if(prime[i]){
+                int pre=i;
+                for(int j=i;j<=n;j+=i){
+                   prime[j]=false;
+                   if(mp[j]>0){//to check whether j is present in our nums array
+                       unionn(pre,j);
+                       pre=j;
+                   }
                 }
-        auto nums2=nums;
-        sort(nums2.begin(),nums2.end());
-        for(int i=0;i<nums.size();i++){
-            if(find(nums[i])!=find(nums2[i]))
-                return false;
+            }
         }
-        
+    }
+    bool gcdSort(vector<int>& nums) {
+        int n=100000;
+        //to check whether j is present in our nums array
+        vector<int>mp(1e5+5,0);
+        for(auto x:nums)mp[x]=1;
+        for(int i=0;i<=100000;i++){parent.push_back(i);size.push_back(1);}
+        vector<int>sortednum=nums;
+        sort(sortednum.begin(),sortednum.end());
+        Sieve(n,nums,mp);
+        for(int i=0;i<nums.size();i++){
+            if(sortednum[i]!=nums[i] && findPar(sortednum[i])!=findPar(nums[i]))return false;
+        }
         return true;
-       
     }
 };
