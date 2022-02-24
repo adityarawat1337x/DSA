787cecea070c30cf80d2f07ce543682425458639
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-24 19:51:03 +0530

    Time: 187 ms (25.27%), Space: 48.7 MB (10.81%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -1,42 +1,48 @@
 
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if (!head || !head->next)
-            return head;
-        ListNode* mid = getMid(head);
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(mid);
-        return merge(left, right);
+    ListNode* getMid(ListNode* head){
+        ListNode* prev=NULL;
+        
+        while(head and head->next){
+            prev = (prev)?prev->next:head;
+            head=head->next->next;
+        }
+        
+        ListNode* h = prev->next;
+        prev->next = nullptr;
+        return h;
     }
-
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        ListNode dummyHead(0);
-        ListNode* ptr = &dummyHead;
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                ptr->next = list1;
-                list1 = list1->next;
-            } else {
-                ptr->next = list2;
-                list2 = list2->next;
+    
+    ListNode* merge(ListNode* l1,ListNode* l2){
+        ListNode* tmp = new ListNode(0);
+        ListNode* ptr = tmp;
+        while(l1 and l2){
+            if(l1->val <l2->val){
+                ptr->next=l1;
+                l1=l1->next;
+            }else{
+                ptr->next=l2;
+                l2=l2->next;
             }
-            ptr = ptr->next;
+            ptr=ptr->next;
         }
-        if(list1) ptr->next = list1;
-        else ptr->next = list2;
-
-        return dummyHead.next;
+        
+        if(l1)
+            ptr->next=l1;
+        else
+            ptr->next=l2;
+        
+        return tmp->next;
     }
-
-    ListNode* getMid(ListNode* head) {
-        ListNode* midPrev = nullptr;
-        while (head && head->next) {
-            midPrev = (midPrev == nullptr) ? head : midPrev->next;
-            head = head->next->next;
-        }
-        ListNode* mid = midPrev->next;
-        midPrev->next = nullptr;
-        return mid;
+    
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        
+        ListNode* mid = getMid(head);
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(mid);
+        return merge(l1,l2);
     }
 };
