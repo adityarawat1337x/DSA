7f4003cf8344a122ece0310b29413079ef5626fa
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-25 23:22:00 +0530

    Time: 2 ms (37.83%), Space: 6.5 MB (10.49%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -5,20 +5,7 @@ public:
             l++;
         return l;
     }
-    int er(string v,int l,int r){
-       
-        return r;
-    }
-    void normalize(string &a,string &b){
-        if(a.size()==b.size())
-            return;
-        else if(a.size()<b.size())
-            normalize(b,a);
-        else
-            while(a.size()!=b.size()){
-                b.push_back('0');
-            }
-    }
+
     int compareVersion(string v1, string v2) {
         
         int i1=0,j1=0,i2=0,j2=0;
@@ -29,21 +16,16 @@ public:
                  j2++;
           
              int sj1 = fr(v1,i1,j1-1);
-             int ej1 = er(v1,sj1,j1-1);
              int sj2 = fr(v2,i2,j2-1);
-             int ej2 = er(v2,sj2,j2-1);
              
-             string a = v1.substr(sj1,ej1-sj1+1);
-             string b = v2.substr(sj2,ej2-sj2+1);
-             //normalize(a,b);
-             //cout<<i1<<" "<<j1<<" "<<a<<" | "<<i2<<" "<<j2<<" "<<b<<"\n";
+             int a = stoi(v1.substr(sj1,j1-sj1));
+             int b = stoi(v2.substr(sj2,j2-sj2));
              
-             if(stoi(a)<stoi(b)){
+             if(a<b)
                  return -1;
-             }
-             if(stoi(b)<stoi(a)){
+             if(b<a)
                  return 1;
-             }
+             
              i1=++j1;
              i2=++j2;
          }
@@ -55,11 +37,8 @@ public:
         }
         
         while(j2<v2.size()){
-            if(v2[j2]!='0' and v2[j2]!='.'){
-              //  cout<<"yay"<<v2[j2];
+            if(v2[j2]!='0' and v2[j2]!='.')
                 return -1;                
-            }
-            
             j2++;
         }
         
