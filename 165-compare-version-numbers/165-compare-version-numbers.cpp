f01ce50d8c21343d1e232b3c2a1cf5f99a8f5089
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-25 23:25:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (73.32%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int fr(string v,int l,int r){
-        while(l<r and v[l]=='0')
-            l++;
-        return l;
-    }
+
 
     int compareVersion(string v1, string v2) {
     int i = 0 ; 
         int j = 0 ; 
         while(i<v1.length() || j<v2.length()){
-            // convert the string into int and store in x and y respectivley 
-			// advantage : problem of trailing 0's eliminated 
 			int x = 0 ;
             int y = 0 ; 
 			
-			// index out of bound check. If not then convert that part into int 
             while(i < v1.length() && v1[i] != '.'){
                 x = x*10 + v1[i]-'0' ;  i++ ; 
             }
 			
-			// same as above 
             while(j < v2.length() &&  v2[j] != '.'){
                 y = y*10 + v2[j]-'0' ; j++ ; 
             }
 			
-			// currently i and j pointing at '.' increment it for pointing to next part  
             i++ ; j++ ; 
-		 
 			
             if(x < y) return -1 ; 
             else if(x>y) return 1 ; 
