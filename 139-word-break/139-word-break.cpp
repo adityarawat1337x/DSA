0d12f1a217178657e6899c4a1b792cf2e15672da
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-25 11:54:48 +0530

    Time: 15 ms (55.04%), Space: 14.7 MB (21.25%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -5,21 +5,19 @@ public:
         for(auto i:wordDict)
             dict.insert(i);
         
-        long dp[s.size()];
+        long dp[s.size()+1];
         memset(dp,0,sizeof(dp));
-        
-        for(int i=0;i<s.size();i++){
+        dp[0]=1;
+        for(int i=1;i<=s.size();i++){
             for(int j=0;j<=i;j++){
-                string search = s.substr(j,i-j+1);
-                if(dict.count(search)){
-                    if(j>0)
-                        dp[i] = dp[i] or (dp[j-1] and 1);
-                    else 
-                        dp[i] = 1;
+                string search = s.substr(j,i-j);
+                if(dp[j] and dict.count(search)){
+                    dp[i] = dp[j];
+                    break;
                 }
             }
         }
         
-        return dp[s.size()-1];
+        return dp[s.size()];
     }
 };
