9fab4f8278cb28e7c034a99d4b6f4f8cc407e065
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-25 13:42:36 +0530

    Time: 88 ms (88.57%), Space: 51.2 MB (5.01%) - LeetHub

---
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<vector<int>> dp(arr.size()+1);
+        stack<int> st;
+        vector<int> l(arr.size()),r(arr.size());
+        
+        for(int i=0;i<arr.size();i++){
+            while(!st.empty() and arr[st.top()]>arr[i])
+                st.pop();
+            if(!st.empty())
+                l[i] = st.top();
+            else
+                l[i]=-1;
+            st.push(i);
+        }
+
+        while(!st.empty())
+            st.pop();
+        
+        for(int i=arr.size()-1;i>-1;i--){
+            while(!st.empty() and arr[st.top()]>=arr[i])
+                st.pop();
+            if(!st.empty())
+                r[i] = st.top();
+            else
+                r[i]=arr.size();
+            st.push(i);
+        }
+
+        
+        long long ans=0,mod = 1e9 + 7;
+        
+        for(int i=0;i<arr.size();i++){
+            //cout<<l[i]<<" "<<i<<" "<<r[i]<<"\n";
+            ans=(ans%mod + (arr[i]%mod*((i-l[i])%mod*(r[i]-i)%mod)%mod)%mod)%mod;
+        }
+        
+        return ans;
+    }
+};
