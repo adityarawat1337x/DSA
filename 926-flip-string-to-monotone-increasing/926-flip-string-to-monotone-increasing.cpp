c292cad3a8c9e6be80bc88e9d99934e7eda0c33f
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-25 08:34:44 +0530

    Time: 36 ms (61.39%), Space: 11 MB (80.02%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int cntOnes=0,cntZeros=0;
-        int ans=INT_MAX;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='0')
-                cntZeros++;
-        }
         
          for(int i=0;i<s.size();i++){
-            if(s[i]=='0')
-                cntZeros--;
-            ans = min((cntZeros+cntOnes),ans);
-            if(s[i]=='1')
+           
+             if(s[i]=='0')
+                cntZeros = min(1+cntZeros,cntOnes);
+             else
                 cntOnes++;
+             
         }
-        return ans;
+        return cntZeros;
     }
 };
