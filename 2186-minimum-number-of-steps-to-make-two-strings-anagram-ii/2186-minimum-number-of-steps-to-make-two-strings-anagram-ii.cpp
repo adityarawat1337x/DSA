c110d779474326d715e7b00e9a884ea5f5a0007a
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-02-27 17:25:18 +0530

    Time: 438 ms (11.11%), Space: 29.1 MB (22.22%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        if(s.size()<t.size())
-            return minSteps(t,s);
         unordered_map<char,int> mp;
-        unordered_map<char,int> mp2;
         
         for(auto i:s){
             mp[i]++;
@@ -16,7 +13,6 @@ public:
         
         int ans=0;
         for(char c='a';c<='z';c++){
-            int sc=mp[c],tc=mp2[c];
             ans+=abs(mp[c]);
         }
         
