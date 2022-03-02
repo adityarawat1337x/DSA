9f2420ab62467b2bfa9bbe1f9aef571cdb3f29e4
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-02 18:04:42 +0530

    Time: 177 ms (54.72%), Space: 53.9 MB (84.91%) - LeetHub

---
+++ 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumSubseqWidths(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        long maxi=0,mini=0;
+        long mod=1e9 + 7,power=1;
+        int n=nums.size()-1;
+
+        for(int i=0;i<nums.size();i++,power=power*2%mod){
+            maxi = (maxi + (long)((power-1)*nums[i]));
+            maxi=maxi%mod;
+            mini = (mini + (long)((power-1)*nums[n-i]));
+            mini=mini%mod;
+        }
+ 
+        
+        long out = maxi-mini;
+        if(out<0)
+            out+=mod;
+        return out;
+    }
+};
