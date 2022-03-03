21ebdd3080e054d9752723f27e36f6d30888711c
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-03 16:22:26 +0530

    Time: 42 ms (25.40%), Space: 17.8 MB (65.50%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,8 +1,9 @@
-class Solution {
-public:
-    int candy(vector<int>& rat) {
+/*
         vector<pair<int,int>> v;
-        vector<int> fill(rat.size(),0);
+              vector<int> fill(rat.size(),0);
+        for(int i=0;i<rat.size();i++){
+        
+        }
         for(int i=0;i<rat.size();i++){
             v.push_back({rat[i],i});
         }
@@ -17,8 +18,8 @@ public:
             if(idx-1>=0){
                 if(rat[idx-1]>rat[idx])
                      fill[idx-1]=max(fill[idx-1],fill[idx]);
-               
             }
+            
             if(idx+1<rat.size()){
                 if(rat[idx+1]>rat[idx])
                     fill[idx+1]=max(fill[idx+1],fill[idx]);
@@ -28,7 +29,26 @@ public:
         int ans=0;
         for(auto i:fill)
             ans+=i;
+        return ans;
+*/
+
+class Solution {
+public:
+    int candy(vector<int>& rat) {
+        vector<int> fill(rat.size(),1);
+        for(int i=1;i<rat.size();i++){
+            if(rat[i]>rat[i-1])
+                fill[i]=fill[i-1]+1;
+        }
         
+        
+        for(int i=rat.size()-2;i>-1;i--){
+            if(rat[i]>rat[i+1])
+                fill[i]=max(fill[i],fill[i+1]+1);
+        }
+        int ans=0;
+         for(auto i:fill)
+            ans+=i;
         return ans;
     }
 };
