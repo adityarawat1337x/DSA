042576c82ff6b99b5ccd2a3189af3c86e97f829d
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-04 07:51:35 +0530

    Time: 104 ms (81.13%), Space: 80.5 MB (71.44%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -3,11 +3,13 @@ public:
     int minSwaps(vector<int>& s) {
   
        int cntOnes=0,cntZero=0,ans=INT_MAX,swap=0;
-        
+        //count total ones
         for(int i=0;i<s.size();i++){
              if(s[i]==1)
                 cntOnes++;
         }
+        
+        // for ones
         for(int i=0;i<cntOnes;i++){
             if(s[i]==0)
                 swap++;
