2d3e25e5643a93a1e56192d6387ac94a435caaca
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-13 19:13:54 +0530

    Time: 3 ms (34.81%), Space: 6.3 MB (50.80%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int valid(int i,int j,int m ,int n){
-        if(i<0 or j<0 or i>=m or j>=n)
-            return false;
-        return true;
-    }
-    int uniquePaths(int m, int n) {
-        int ans=0,dp[m][n];
-        memset(dp,0,sizeof(dp));
-        dp[m-1][n-1]=1;
-        for(int i=m-1;i>-1;i--){
-            for(int j=n-1;j>-1;j--){
-                if(valid(i+1,j,m,n))
-                    dp[i][j]+=dp[i+1][j];
-                
-            
-                if(valid(i,j+1,m,n))
-                    dp[i][j]+=dp[i][j+1];
-                
-            }
-        }
     
-        return dp[0][0];
+    int uniquePaths(int m,int n) {
+        vector<long> row(n+1);
+        
+        row[n-1]=1;
+        for(int i=m-1;i>-1;i--){
+            vector<long> tmp(n+1);
+            for(int j=n-1;j>-1;j--){
+                tmp[j] = row[j] + tmp[j+1];
+            }
+            row=tmp;
+        }
+            
+        return row[0];
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
