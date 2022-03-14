eadf714430858b07b268eae7cc3e4083f3a7055a
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-14 08:07:40 -0700

    Time: 31 ms (14.73%), Space: 10.7 MB (5.06%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*RECURSION + MEMO
     int run(int i,int j,vector<vector<int>>& mat,vector<vector<long>> &dp){
         if(j<0 or j==mat[0].size())
             return INT_MAX;
@@ -22,6 +23,30 @@ public:
         vector<vector<long>> dp(mat.size(),vector<long>(mat[0].size(),INT_MAX));
         for(int i=0;i<mat[0].size();i++)
             ans = min(ans,run(0,i,mat,dp));
+        return ans;
+    }
+    */
+  
+    
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        long ans=INT_MAX;
+        vector<vector<long>> dp(mat.size()+1,vector<long>(mat[0].size()+2,INT_MAX));
+          
+        for(int j=0;j<=mat[0].size()+1;j++){
+              dp[0][j]=0;
+          }
+        for(int i=1;i<=mat.size();i++){
+            for(int j=1;j<=mat[0].size();j++){
+                int up = dp[i-1][j];        
+                int right = dp[i-1][j+1];
+                int left = dp[i-1][j-1];
+                dp[i][j] = mat[i-1][j-1] + min({right,left,up});
+            }
+        }
+          for(int j=1;j<=mat[0].size();j++){
+              ans=min(ans,dp[mat.size()][j]);
+          }
+        
         return ans;
     }
 };
