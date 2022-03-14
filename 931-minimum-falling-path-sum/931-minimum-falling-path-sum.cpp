c5f374f79e74540432d28c3ecd16f061063fb39c
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-14 08:09:25 -0700

    Time: 23 ms (37.84%), Space: 10.7 MB (5.06%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -30,21 +30,20 @@ public:
     
     int minFallingPathSum(vector<vector<int>>& mat) {
         long ans=INT_MAX;
-        vector<vector<long>> dp(mat.size()+1,vector<long>(mat[0].size()+2,INT_MAX));
-          
-        for(int j=0;j<=mat[0].size()+1;j++){
-              dp[0][j]=0;
-          }
+        vector<long> row(mat[0].size()+2,0);
+
         for(int i=1;i<=mat.size();i++){
+             vector<long> tmp(mat[0].size()+2,INT_MAX);
             for(int j=1;j<=mat[0].size();j++){
-                int up = dp[i-1][j];        
-                int right = dp[i-1][j+1];
-                int left = dp[i-1][j-1];
-                dp[i][j] = mat[i-1][j-1] + min({right,left,up});
+                int up = row[j];        
+                int right =row[j+1];
+                int left = row[j-1];
+                tmp[j] = mat[i-1][j-1] + min({right,left,up});
             }
+            row=tmp;
         }
           for(int j=1;j<=mat[0].size();j++){
-              ans=min(ans,dp[mat.size()][j]);
+              ans=min(ans,row[j]);
           }
         
         return ans;
