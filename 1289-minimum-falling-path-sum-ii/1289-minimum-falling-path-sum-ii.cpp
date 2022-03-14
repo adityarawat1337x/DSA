77dbdbc8e631688d0c6257d76a085cb573e198a3
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-14 08:14:27 -0700

    Time: 258 ms (56.57%), Space: 16.7 MB (8.29%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-
+    /* RECURSIVE + MEMO
     int run(int i,int j,vector<vector<int>>& mat,vector<vector<long>> &dp){
         if(i==mat.size()-1){
             return mat[i][j];
@@ -28,6 +28,30 @@ public:
             int val = run(0,i,mat,dp);
             ans = min(ans,val);
         }
+        return ans;
+    }
+    */
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        long ans=INT_MAX;
+        vector<long> row(mat[0].size()+2,0);
+
+        for(int i=1;i<=mat.size();i++){
+            vector<long> tmp(mat[0].size()+2,INT_MAX);
+            for(int j=1;j<=mat[0].size();j++){
+                long mini = INT_MAX;
+                for(int x=0;x<=mat[0].size();x++){
+                    if(x!=j){
+                        mini = min(mini,row[x]); 
+                    }
+                }
+                tmp[j] = mat[i-1][j-1] + mini;
+            }
+            row=tmp;
+        }
+          for(int j=1;j<=mat[0].size();j++){
+              ans=min(ans,row[j]);
+          }
+        
         return ans;
     }
 };
