892ba043346da2aee35f342a1e9c8759a57035d4
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-15 22:14:55 +0530

    Time: 568 ms (10.79%), Space: 11.9 MB (59.36%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -8,20 +8,22 @@ public:
             return false;
         int target = sum/2;
         
-        vector<vector<bool>> dp(target+1,vector<bool>(nums.size(),0));
+        vector<vector<bool>> dp(nums.size(),vector<bool>(target+1,0));
         
-        for(int i=0;i<dp[0].size();i++)
-            dp[0][i]=1;
+        for(int i=0;i<dp.size();i++)
+            dp[i][0]=1;
+        //dp[0][nums[0]]=1;
         
-    for(int i=1;i<=target;i++)
-        for(int j=1;j<dp[0].size();j++){
-            bool no = dp[i][j-1];
-            bool yes = false;
-            if(nums[j]<=i){
-                yes = dp[i-nums[j]][j-1];
+        for(int i=1;i<dp.size();i++)
+            for(int j=1;j<=target;j++){
+                bool no = dp[i-1][j];
+                bool yes = false;
+                if(nums[i]<=j){
+                    yes = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = yes or no;
             }
-            dp[i][j] = yes or no;
-        }
+            
         
         return dp.back().back();
     }
