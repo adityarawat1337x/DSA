404438474e258d174797fb4f06a8a312efbb16c2
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-16 23:23:45 +0530

    Time: 7 ms (74.97%), Space: 13.9 MB (11.56%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,15 +4,14 @@ public:
         int n = cost.size();
         if(n==1)
             return cost[0];
-        cost.push_back(0);
         vector<int> dp(n+1);
         dp[0]=cost[0];
         dp[1]=cost[1];
         
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<n;i++){
             dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
         }
         
-        return dp.back();
+        return min(dp[n-1],dp[n-2]);
     }
 };
