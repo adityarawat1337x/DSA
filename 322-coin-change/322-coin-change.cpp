715624bc9ecafc0d5b142f0be79c0b9ebccd663c
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-21 18:45:59 +0530

    Time: 132 ms (42.55%), Space: 49.3 MB (10.45%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,7 @@
 #define vi vector<int> 
 class Solution {
 public:
+    /* RECUR + MEMO
     int minCoin(vector<int>& coins,int idx,int amount,vector<vi> &dp) {
         if(amount==0)
             return 0;
@@ -18,10 +19,27 @@ public:
         
         return dp[idx][amount] = min(take,noTake);
     }
-    
+    */
     int coinChange(vector<int>& coins, int amount) {
-        vector<vi> dp(coins.size(),vi(amount+1,-1));
-        int ans =  minCoin(coins,coins.size()-1,amount,dp);
-        return ans>=INT_MAX-1?-1:ans;
+        vector<vi> dp(coins.size()+1,vi(amount+1,-1));
+        for(int i=0;i<=amount;i++)
+            dp[0][i]=INT_MAX-1;
+        
+        for(int i=0;i<=coins.size();i++)
+            dp[i][0]=0;
+        
+        for(int i=1;i<=coins.size();i++)
+            for(int j=1;j<=amount;j++){
+                int noTake = dp[i-1][j];
+                int take = INT_MAX-1;
+                if(j>=coins[i-1])
+                    take = 1 + dp[i][j-coins[i-1]];
+                dp[i][j] = min(take,noTake);
+            }
+        
+        return dp.back().back()>=INT_MAX-1?-1:dp.back().back();
+        
+        //int ans =  minCoin(coins,coins.size()-1,amount,dp);
+        //return ans>=INT_MAX-1?-1:ans;
     }
 };
