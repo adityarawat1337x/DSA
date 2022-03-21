784e444a336f5efed8ead9ea3b4664626d637653
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-21 19:31:12 +0530

    Time: 12 ms (85.52%), Space: 7.2 MB (86.20%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -2,20 +2,15 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vi dp(amount+1);
-        for(int i=0;i<=amount;i++)
-            dp[i]=0;
-        
-        for(int i=1;i<=coins.size();i++){
-            dp[0]=1;
-            for(int j=1;j<=amount;j++){
-                int noTake = dp[j];
-                int take = 0;
-                if(j>=coins[i-1])
-                    take = dp[j-coins[i-1]];
-                dp[j] = take+noTake;
-            }
-        }
-        return dp.back();
+    vector<int> ways(amount + 1, 0);
+    ways[0] = 1;
+
+    for (int coin : coins) {
+      for (int i = coin; i <= amount; i++) {
+        ways[i] += ways[i - coin];
+      }
     }
+
+    return ways[amount];
+  }
 };
