d1ab19ae0e0bc1f9d0a4eb884da7424277cc7397
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-23 16:49:26 +0530

    Time: 9 ms (36.07%), Space: 12.1 MB (15.82%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
+      // RECUSRSION + MEMO
+    int recur(vector<int>& prices,vector<vector<int>> &dp,int idx,bool buy){
+        if(idx>=prices.size())
+            return 0;
+        if(dp[idx][buy]!=-1)
+            return dp[idx][buy];
+        
+        if(buy){
+            return dp[idx][buy] = max(recur(prices,dp,idx+1,!buy)-prices[idx],recur(prices,dp,idx+1,buy));
+        }
+        return dp[idx][buy] = max(recur(prices,dp,idx+1,buy),recur(prices,dp,idx+2,!buy)+prices[idx]);
+    }
+    
     int maxProfit(vector<int>& prices) {
-        int buy=0,sell=INT_MIN,prev=buy;
+        /*int sell=0,buy=INT_MIN,prev=buy;
+     
         for(auto p:prices){
             int prev2=prev;
-            prev=buy;
+            prev=sell;
             buy=max(buy,sell+p);
             sell=max(sell,prev2-p);
         }
         
-        return buy;
+        return sell;
+        */
+        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+        return recur(prices,dp,0,1);
+
     }
 };
+
+   //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75924/Most-consistent-ways-of-dealing-with-the-series-of-stock-problems
