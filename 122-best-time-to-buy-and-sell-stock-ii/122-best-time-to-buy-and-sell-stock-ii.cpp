8c15f5a4b9d46ddad54a33dc6bfef0a91a4fa3a9
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-23 18:02:47 +0530

    Time: 11 ms (39.03%), Space: 15.1 MB (5.91%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* RECUSRSION + MEMO
     int recur(vector<int>& prices,vector<vector<int>> &dp,int idx,bool buy){
         if(idx>=prices.size())
             return 0;
@@ -11,16 +12,19 @@ public:
         }
         return dp[idx][buy] = max(recur(prices,dp,idx+1,buy),recur(prices,dp,idx+1,!buy)+prices[idx]);
     }
+    
+    */
+    
     int maxProfit(vector<int>& prices) {
         vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
         
-        int sell=INT_MIN,buy=0;
+        //TABULATION + SPACE OPTIMIZATION
+        int sell=0,buy=INT_MIN;
         for(auto p:prices){
-            int prev=buy;
-            sell = max(sell,prev-p);
-            buy = max(buy,sell+p);
+            buy = max(buy,sell-p);
+            sell = max(sell,buy+p);
         }
-        return buy;
+        return sell;
         //return recur(prices,dp,0,1);
     }
 };
