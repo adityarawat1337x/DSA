fd1a9c4dd95c3b43cfbf5670c941f6faf4e0b904
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-24 19:04:02 +0530

    Time: 44 ms (60.27%), Space: 30 MB (28.31%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -9,17 +9,30 @@ public:
         int ans=0;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int yes =0,no=0;
         
         if(t1[i]==t2[j])
-            yes = solve(t1,t2,i-1,j-1,dp) + solve(t1,t2,i-1,j,dp);
+            dp[i][j] = solve(t1,t2,i-1,j-1,dp) + solve(t1,t2,i-1,j,dp);
         else {
-             no = solve(t1,t2,i-1,j,dp);
+             dp[i][j] = solve(t1,t2,i-1,j,dp);
         }
-        return dp[i][j] =yes+no;
+        return dp[i][j];
     }
     
     int numDistinct(string t1, string t2) {
+        //TABULATION
+        vector<vector<double>> dp(t1.size()+1,vector<double>(t2.size()+1,1));
+        for(int i=1;i<=t1.size();i++){
+            for(int j=1;j<=t2.size();j++){
+                dp[0][j]=0;
+                if(t1[i-1]==t2[j-1])
+                     dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }  
+        }       
+        
+        return dp.back().back();
+        
         //RECUR + MEMO
         vector<vector<int>> memo(t1.size(),vector<int>(t2.size(),-1));
         return solve(t1,t2,t1.size()-1,t2.size()-1,memo);
