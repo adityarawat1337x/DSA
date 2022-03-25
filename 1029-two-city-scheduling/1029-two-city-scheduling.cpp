edafa834185d34fd6927332b5b4542e30ed21cd0
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-25 11:24:38 +0530

    Time: 3 ms (90.96%), Space: 7.8 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,27 +1,14 @@
+bool comparator ( vector <int> &a, vector <int> &b ){
+    if( a[0] - a[1]  < b[0] - b[1]) return true; // sort by difference in cost
+    else return false;
+}
+
 class Solution {
-public:
-    int rec(vector<vector<int>>& costs,int idx,int a,int b,int &n,vector<vector<vector<long long>>> &dp){
-         if (idx==costs.size() and a==b)
-            return 0;
-        
-        if(idx>=costs.size() or a>n or b>n)
-            return 1e9;
-        
-        if(dp[idx][a][b]!=INT_MAX)
-            return dp[idx][a][b];
-        
-        long long ans=0;
-        long long chooseA = rec(costs,idx+1,a+1,b,n,dp);
-        long long chooseB = rec(costs,idx+1,a,b+1,n,dp);
-        
-   
-        return dp[idx][a][b] = min(costs[idx][0]+chooseA,costs[idx][1]+chooseB);
-    }
-    
+public:   
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size()/2;
-        vector<vector<vector<long long>>> dp(costs.size(),vector<vector<long long>> (n+1,vector<long long>(n+1,INT_MAX)));
-        
-        return rec(costs,0,0,0,n,dp);
+        sort(costs.begin(), costs.end(), comparator);
+        int n = costs.size()/2, ans = 0;
+        for( int i = 0 ; i < n ; i++ ) ans += costs[i][0] + costs[i+n][1]; // send the first to a , rest to b
+        return ans;
     }
 };
