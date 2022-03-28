5ef66a2fffc9a76b13d792f4bc2c36904d440772
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-28 07:26:05 +0530

    Time: 3 ms (95.97%), Space: 14.1 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-      bool bs(vector<int>& nums, int target,int l,int r) {
-        
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(nums[m]==target)
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return false;
+        int end = n - 1;
+        int start = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
                 return true;
-            if(nums[m]>target)
-                r=m-1;
-            else l=m+1;
+            }
+
+            if (!isBinarySearchHelpful(nums, start, nums[mid])) {
+                start++;
+                continue;
+            }
+
+            // which array does pivot belong to.
+            bool pivotArray = existsInFirst(nums, start, nums[mid]);
+
+            // which array does target belong to.
+            bool targetArray = existsInFirst(nums, start, target);
+            if (pivotArray ^ targetArray) { // If pivot and target exist in different sorted arrays, recall that xor is true only when both the operands are distinct
+                if (pivotArray) {
+                    start = mid + 1; // pivot in the first, target in the second
+                } else {
+                    end = mid - 1; // target in the first, pivot in the second
+                }
+            } else { // If pivot and target exist in same sorted array
+                if (nums[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
         }
-        
         return false;
     }
-    
-    bool search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        
-        while(l<r){
-            if(nums[l]>nums[l+1])
-                break;
-            l++;
-        }
-        
-        return bs(nums,target,0,l) or bs(nums,target,l+1,r);
+
+    // returns true if we can reduce the search space in current binary search space
+    bool isBinarySearchHelpful(vector<int>& nums, int start, int element) {
+        return nums[start] != element;
+    }
+
+    // returns true if element exists in first array, false if it exists in second
+    bool existsInFirst(vector<int>& nums, int start, int element) {
+        return nums[start] <= element;
     }
 };
