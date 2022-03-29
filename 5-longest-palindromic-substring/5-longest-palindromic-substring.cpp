17f30afaefc7fda6c1934ee0e6ea37f97ddc19fc
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-30 04:21:12 +0530

    Time: 32 ms (77.15%), Space: 7.6 MB (70.55%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    bool ispal(string &s,int &n){
-    for(int i=0;i<n/2;i++){
-        if(s[i]!=s[n-i-1])
-            return false;
-        }
-        return true;
-    }
+ 
     string longestPalindrome(string s) {
-        int n=s.size();
-        string p=s;
-        reverse(p.begin(),p.end());
-        int lcs[n+1][n+1];
-        int m=-1;
-        for(int i=0;i<=n;i++)
-            for(int j=0;j<=n;j++){
-                if(i==0 || j==0)
-                    lcs[i][j]=0;
-                else if(s[i-1]==p[j-1]){
-                        lcs[i][j]=1+lcs[i-1][j-1];
-                        m=max(m,lcs[i][j]);
-                }
-                else
-                    lcs[i][j]=0;
-                }
-        
-        cout<<m;
-        while(m){
-            for(int i=0;i+m<=n;i++){
-                string t = s.substr(i,m);
-                if(ispal(t,m)){
-                    return t;
-                }
+        int len=1;
+        string ans=s.substr(0,len);
+        for(int i=0;i<s.size();i++){
+            int l=i-1,r=i+1;
+            while(l>-1 and s[l]==s[l+1])
+                l--;
+            while(r<s.size() and s[r]==s[r-1]){
+                r++;
             }
-            m--;
+            while(l>-1 and r<s.size() and (s[l]==s[r])){
+                l--;
+                r++;
+            }
+          
+            if(r-l-1>len){
+                len=r-l-1;
+                ans = s.substr(l+1,len);
+            }
         }
-               
-        return "";
+        return ans;
     }
 };
