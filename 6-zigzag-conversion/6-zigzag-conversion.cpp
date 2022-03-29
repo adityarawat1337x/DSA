c82e462bd7e9d4c431c8c8eb7f4480adbab4931f
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-03-29 17:51:20 +0530

    Time: 8 ms (86.87%), Space: 8.3 MB (63.77%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -3,14 +3,13 @@ public:
     string convert(string s, int row) {
         int i=0,j=0,shift=2*row-2,ctr=0;
         string ans="";
+        if(shift==0)
+            return s;
+        
         while(ctr<s.size()){
             ans+=s[i];
             ctr++;
-            if(shift==0){
-                j++;
-                i=j;
-                continue;
-            }
+            
             i+=shift;
             if(j>0 and j<row-1 and i-2*j<s.size()){
                 ans+=s[i-2*j];
