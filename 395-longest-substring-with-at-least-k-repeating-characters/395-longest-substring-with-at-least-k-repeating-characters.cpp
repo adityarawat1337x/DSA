9069bdb7e448a5164158a9d3546e536dc3abf0c6
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-04-18 10:20:58 +0530

    Time: 39 ms (34.39%), Space: 6.7 MB (58.86%) - LeetHub

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+   void longestWithLengthI(string &s,int &k,int &unique,int &ans){
+       int uniqueWithK=0,uniqueSoFar=0;
+       unordered_map<int,int> mp;
+       for(int i=0,j=0;j<s.size();j++){
+          if(mp[s[j]]==0){
+               uniqueSoFar++;
+           }
+           mp[s[j]]++;
+          
+           if(mp[s[j]]==k){
+               uniqueWithK++;
+           }
+           
+           while(uniqueSoFar>unique){
+               if (mp[s[i]] == k) 
+                    uniqueWithK--;
+               mp[s[i]]--;
+               if(mp[s[i]]==0)
+                   uniqueSoFar--;
+               i++;
+           }
+           
+           if(uniqueSoFar==unique and uniqueWithK==uniqueSoFar){
+               ans = max(ans,j-i+1);
+           }
+       }
+   }
+    
+    int longestSubstring(string s, int k) {
+        int ans=0;
+        for(int i=1;i<27;i++){
+            longestWithLengthI(s,k,i,ans);
+        }
+        return ans;
+    }
+};
