2407bfedc7e8eda89974ebdf020630cea74db106
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-04-23 11:51:42 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (21.39%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -3,17 +3,11 @@ public:
     unordered_map<string,string> mp;
     
     string randomString(){
-        string str="";
-        for(int i=0;i<5;i++)
-            str += (rand()%26 + 'a');
-        return str;
+        return to_string(mp.size()+1);
     }
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
         string str = "http://tinyurl.com/" + randomString();
-        while(mp.find(str)!=mp.end()){
-            str = "http://tinyurl.com/" + randomString();
-        }
         mp[str] = longUrl;
         return str;
     }
