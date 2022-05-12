62eb0f6200a6d594566fde3463baaf0dd8d5f67d
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-05-12 18:46:53 +0530

    Time: 51 ms (64.44%), Space: 7.8 MB (81.11%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    int balancedString(string s) {
-        unordered_map<char,int> mp;
-        int n = s.size()/4;
-        for(auto i:s)
-            mp[i]++;
-
-        unordered_map<char,int> count;
-        set<char> st;
-
-        for(auto i:mp)
-            if(i.second-n>0){
-                count[i.first]=i.second-n;
-                st.insert(i.first);                
-            }
-
-        int ans=INT_MAX;
-        
-        for(int i=0,j=0;j<s.size();j++){
-            if(count.find(s[j])!=count.end()){
-                count[s[j]]--;
-                if(count[s[j]]==0)
-                    st.erase(s[j]);
-                if(st.size()==0)
-                    ans = min(ans,j-i+1);
-                while(st.size()==0){
-                    if(count.find(s[i])!=count.end()){
-                        count[s[i]]++;
-                        if(count[s[i]]>0)
-                            st.insert(s[i]);
-                    }
-                    ans = min(ans,j-i+1);
-                    i++;
-                }
+     int balancedString(string s) {
+        unordered_map<int, int> count;
+        int n = s.length(), res = n, i = 0, k = n / 4;
+        for (int j = 0; j < n; ++j) {
+            count[s[j]]++;
+        }
+        for (int j = 0; j < n; ++j) {
+            count[s[j]]--;
+            while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
+                res = min(res, j - i + 1);
+                count[s[i++]] += 1;
             }
         }
-        return ans==INT_MAX?0:ans;
+        return res;
     }
 };
