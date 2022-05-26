4451cb4b5da8b09a65ff00a07dae4711e46ab2d1
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-05-26 12:02:51 +0530

    Time: 6 ms (17.08%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-
+    int rsb(uint32_t &n){
+        return n&-n;
+    }
     int hammingWeight(uint32_t n) {
-        int k = 0;
-        while(n != 0) {
-            n &= n - 1;
-            k++;
+        int cnt=0;
+        while(n){
+            n-=rsb(n);
+            cnt++;
         }
-        return k;
+        return cnt;
     }
 };
