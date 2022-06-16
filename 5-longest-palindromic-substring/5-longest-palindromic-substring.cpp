4ae3679ba572e818493340a492ff176430affbab
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-06-16 11:53:23 +0530

    Time: 490 ms (25.48%), Space: 30.1 MB (32.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -17,12 +17,11 @@ public:
                 }
             
         
-       for(int i=s.size()-1;i>-1;i--)
-            for(int j=i;j<s.size();j++){
-                if(s[i]==s[j] and j>0 and dp[i+1][j-1]){
+       for(int i=s.size()-2;i>-1;i--)
+            for(int j=i+1;j<s.size();j++){
+                if(s[i]==s[j] and dp[i+1][j-1]){
                     dp[i][j]=true;
                     if(j-i+1 > len){
-                        cout<<i<<" : "<<j<<" , "<<len<<"\n";
                         minl=i;
                         len=j-i+1;
                     }
