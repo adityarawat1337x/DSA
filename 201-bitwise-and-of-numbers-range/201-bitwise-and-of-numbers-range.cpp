98bebdf9026b74072ab7bc6cbb65c3904c4de66d
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-06-19 14:06:53 +0530

    Time: 29 ms (12.47%), Space: 5.9 MB (32.38%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
-    int PositionRightmostSetbit(int n)
-{
-      if(n == 0)
-          return 0;
-    int position = 0;
-    int m = 1;
- 
-    while (!(n & m)) {
-        m = m << 1;
-        position++;
-    }
-    return (1<<position);
-}
-int rangeBitwiseAnd(int left, int right) {
-        int ans=INT_MAX;
-        int near = PositionRightmostSetbit(right);
-        if(left<near){
-            return 0;
+    int rangeBitwiseAnd(int left, int right) {
+        while(left<right){
+            right=right&(right-1);
         }
-        near = max(near,left);
-        while(near<right){
-            ans&=near;
-            if(ans==0)
-                return ans;
-            near++;
-        }
-        return ans&right;
+        return left&right;
     }
 };
