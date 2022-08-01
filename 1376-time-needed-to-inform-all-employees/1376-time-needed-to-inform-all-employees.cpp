c2f758b05868f1b73947e15775cee748f52f7f0a
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-01 23:30:28 +0530

    Time: 234 ms (96.09%), Space: 90.8 MB (91.47%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int dfs(int id, vector<int> v[], vector<int>& time){
-        int ans=0;
-        for(auto i:v[id]){
-            ans = max(ans,time[id]+dfs(i,v,time));
-        }
-        return ans;
-    }
-    
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<int> v[n];
-        for(int i=0;i<manager.size();i++){
-            if(manager[i]==-1)
-                continue;
-            v[manager[i]].push_back(i);
+    int ans=0;
+    int m_time=0;
+    int dfs(int i,vector<int> &manager,vector<int> &informTime){ 
+        if(manager[i]!=-1){
+            informTime[i]+=dfs(manager[i],manager,informTime);
+            manager[i]=-1;
         }
         
-        return dfs(headID,v,informTime);
+        return informTime[i];    
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        int m=manager.size();
+        for(int i=0;i<n;i++){
+            if(manager[i]!=-1)
+            m_time=max(m_time, dfs(i,manager,informTime));
+        }
+
+        return  m_time;
     }
 };
