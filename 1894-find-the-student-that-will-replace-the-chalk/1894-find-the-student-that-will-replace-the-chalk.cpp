3d30e9756b3337f6582150cab4a6cd50b7dc0d00
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-09 00:08:34 +0530

    Time: 184 ms (67.54%), Space: 74.4 MB (45.91%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -4,9 +4,7 @@ public:
         long sum=0;
         for(auto &i:chalk)
             sum+=i;
-        if(k>sum){
-            k=k%sum;
-        }
+        k=k%sum;
         for(int i=0;i<chalk.size();i++){
             if(chalk[i]>k)
                 return i;
