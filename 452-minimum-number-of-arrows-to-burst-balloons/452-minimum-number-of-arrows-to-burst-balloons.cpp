11fb5fcf8328f34724bc44e060bdc51d50854301
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-09 18:47:11 +0530

    Time: 526 ms (84.81%), Space: 89.8 MB (68.21%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,24 +1,15 @@
+bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
 class Solution {
-public:
-    int findMinArrowShots(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(),[](auto &a,auto &b){
-            return (a[1]<b[1] or (a[1]==b[1] and a[0]<b[0]));
-        });
-        vector<vector<int>> res;
-        int l = intervals[0][0];
-        int r = intervals[0][1];
-        for(int i = 1; i < intervals.size(); i++) {
-            if(intervals[i][0] <= r) {
-                if(intervals[i][1] > r)
-                    r = min(r,intervals[i][1]);
-                    l = min(l,intervals[i][0]);
-            } else {
-                res.push_back({l, r});
-                l = intervals[i][0];
-                r = intervals[i][1];
+public:  
+    int findMinArrowShots(vector<vector<int>>& segments) {
+        sort(segments.begin(), segments.end(), cmp);
+        int ans = 0, arrow = 0;
+        for (int i = 0; i < segments.size(); i ++) {
+            if (ans == 0 || segments[i][0] > arrow) {
+                ans ++;
+                arrow = segments[i][1];
             }
         }
-        res.push_back({l, r});
-        return res.size();
+        return ans;
     }
 };
