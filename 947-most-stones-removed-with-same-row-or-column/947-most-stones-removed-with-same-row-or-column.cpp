08c8fae5bf35b3d4a423433aa3b80d8de5b9f6ad
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-14 00:12:05 +0530

    Time: 61 ms (89.42%), Space: 16.4 MB (52.46%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-   int removeStones(vector<vector<int>>& stones) {
-        for (int i = 0; i < stones.size(); ++i)
-            uni(stones[i][0], ~stones[i][1]);
-        return stones.size() - islands;
+     unordered_map<int,int> parent;
+     int n,cnt=0;
+    
+    int findParent(int x){
+        if (!parent.count(x)) parent[x] = x, cnt++;
+        if(parent[x]!=x)
+            return parent[x]=findParent(parent[x]);
+        return x;
     }
-
-    unordered_map<int, int> par;
-    int islands = 0;
-
-    int find(int x) {
-        if (!par.count(x)) par[x] = x, islands++;
-        if (x != par[x]) par[x] = find(par[x]);
-        return par[x];
+    
+    void Union(int i,int j){
+        int x = findParent(i),  y = findParent(j);
+        if(x!=y)        
+            cnt--,parent[x]=y;
     }
-
-    void uni(int x, int y) {
-        x = find(x), y = find(y);
-        if (x != y) par[x] = y, islands--;
+    
+    int removeStones(vector<vector<int>>& edges) {
+        
+        for(auto &e:edges){
+            Union(e[0],~e[1]);
+        }
+ 
+        return edges.size()-cnt;
     }
 };
