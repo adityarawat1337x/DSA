4f36981cd8b29ebf74cbe5da0017ccfe3e4d4da4
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-14 13:55:38 +0530

    Time: 11 ms (47.47%), Space: 11.4 MB (73.30%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    unordered_map<char,char> par;
-    char find(char x){
-        char y=x;
+    int par[26];
+    int find(int x){
+        int y=x;
         while(y!=par[y])
             y=par[y];
         return par[x]=y;
     }
     
-    void Union(char a, char b){
+    void Union(int a, int b){
         a=find(a),b=find(b);
         par[a]=b;
     }
     
     bool equationsPossible(vector<string>& eq) {
         for(char c='a';c<='z';c++)
-            par[c]=c;
+            par[c-'a']=c-'a';
         
         for(auto &s:eq){
             char a=s[0],b=s[3],c=s[1];
-            if(s[1]=='='){
-                Union(a,b);
+            if(c=='='){
+                Union(a-'a',b-'a');
             }else{
-                a=find(a),b=find(b);
-                if(a==b)
+                int x=find(a-'a'),y=find(b-'a');
+                if(x==y)
                     return false;
             }
         }
+        
         reverse(eq.begin(),eq.end());
         
-        for(auto &s:eq){
+         for(auto &s:eq){
             char a=s[0],b=s[3],c=s[1];
-            if(s[1]=='='){
-                Union(a,b);
+            if(c=='='){
+                Union(a-'a',b-'a');
             }else{
-                a=find(a),b=find(b);
-                if(a==b)
+                int x=find(a-'a'),y=find(b-'a');
+                if(x==y)
                     return false;
             }
         }
-        
         return true;
     }
 };
