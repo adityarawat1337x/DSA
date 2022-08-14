0d1b65e47199f5e4a5a886fb98abae841f393a68
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-14 13:04:16 +0530

    Time: 38 ms (39.44%), Space: 9.1 MB (83.90%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,80 +1,67 @@
-class Solution {
+class Solution
+{
 public:
-bool able(string s,string t){
-    if(s.length()!=t.length())
-        return false;
-    int c=0;
-    for(int i=0;i<s.length();i++)
-        c+=(s[i]!=t[i]);
-    return c==1;
-}
-void bfs(vector<vector<int>> &g,vector<int> parent[],int n,int sr,int ds){
-    vector <int> dist(n,1005);
-    queue <int> q;
-    q.push(sr);
-    parent[sr]={-1};
-    dist[sr]=0;
-    while(!q.empty()){
-        int x=q.front();
-        q.pop();
-        for(int u:g[x]){
-            if(dist[u]>dist[x]+1){
-                dist[u]=dist[x]+1;
-                q.push(u);
-                parent[u].clear();
-                parent[u].push_back(x);
-            }
-            else if(dist[u]==dist[x]+1)
-                parent[u].push_back(x);
+    vector<vector<string>> res;
+    vector<string> te;
+    unordered_map<string, int> mp;
+    string b;
+    void dfs(string s)  // Step 2
+    {
+        te.push_back(s);
+        if (s == b)
+        {
+            vector<string> x = te;
+            reverse(x.begin(), x.end());
+            res.push_back(x);
+            te.pop_back();
+            return;
         }
-    }
-}
-void shortestPaths(vector<vector<int>> &Paths, vector<int> &path, vector<int> parent[],int node){
-    if(node==-1){
-        Paths.push_back(path);
-        return ;
-    }
-    for(auto u:parent[node]){
-        path.push_back(u);
-        shortestPaths(Paths,path,parent,u);
-        path.pop_back();
-    }
-}
-vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-    int n=wordList.size(),sr=-1,ds=-1;
-    vector<vector<string>> ANS;
-    for(int i=0;i<n;i++){
-        if(wordList[i]==beginWord)
-            sr=i;
-        if(wordList[i]==endWord)
-            ds=i;
-    }
-    if(ds==-1)
-        return ANS;
-    if(sr==-1){
-        wordList.emplace(wordList.begin(),beginWord);
-        sr=0;
-        ds++;
-        n++;
-    }
-    vector <vector<int>> g(n,vector<int>()),Paths;
-    vector <int> parent[n],path;
-    for(int i=0;i<n-1;i++)
-        for(int j=i+1;j<n;j++)
-            if(able(wordList[i],wordList[j])){
-                g[i].push_back(j);
-                g[j].push_back(i);
+        int cur = mp[s];
+        for (int i = 0; i < s.size(); i++)
+        {
+            char c = s[i];
+            for (char cc = 'a'; cc <= 'z'; cc++)
+            {
+                s[i] = cc;
+                if (mp.count(s) && mp[s] == cur - 1)
+                    dfs(s);
             }
-    bfs(g,parent,n,sr,ds); 
-    shortestPaths(Paths,path,parent,ds);
-    for(auto u:Paths){
-        vector <string> now;
-        for(int i=0;i<u.size()-1;i++)
-            now.push_back(wordList[u[i]]);
-        reverse(now.begin(),now.end());
-        now.push_back(wordList[ds]);
-        ANS.push_back(now);
+            s[i] = c;
+        }
+        te.pop_back();
+        return;
     }
-    return ANS;
-}
-}; 
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
+    {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        b = beginWord;
+        queue<string> q;
+        int k = beginWord.size();
+        q.push({beginWord});
+        mp[beginWord] = 0;
+        while (!q.empty())  // Step 1
+        {
+            int n = q.size();
+            while (n--)
+            {
+                string t = q.front();
+                q.pop();
+                int x = mp[t] + 1;
+                for (int i = 0; i < k; i++)
+                {
+                    string temp = t;
+                    for (char ch = 'a'; ch <= 'z'; ch++)
+                    {
+                        temp[i] = ch;
+                        if (!mp.count(temp) && dict.count(temp))
+                            mp[temp] = x, q.push(temp);
+                    }
+                }
+            }
+        }
+        if (mp.count(endWord))
+            dfs(endWord);
+        return res;
+    }
+};
+
