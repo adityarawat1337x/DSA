ea9383cd62d20179183a2002091cffd2d5cd8401
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-18 01:49:12 +0530

    Time: 1639 ms (26.47%), Space: 178.3 MB (21.97%) - LeetHub

--- 805-split-array-with-same-average/805-split-array-with-same-average.cpp
+++ 805-split-array-with-same-average/805-split-array-with-same-average.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    vector<vector<vector<bool>>>dp;
-    bool possible(vector<int> &A, int index, int count, int sum){
-        if(sum == 0 && count == 0)  return true;
-        if(sum < 0 || index >= A.size() || count == 0)    return false;
-        if(dp[index][count][sum] == false)  return false;
-        if(possible(A, index + 1, count - 1, sum - A[index]))   return dp[index][count][sum] = true;
-        if(possible(A, index + 1, count, sum))  return dp[index][count][sum] = true;
-        return dp[index][count][sum] = false;
-    }
-    bool splitArraySameAverage(vector<int>& A) {
-        int n = A.size();
-        int sum = 0;
-        sum = accumulate(A.begin(), A.end(), sum);
-        dp.resize(n, vector<vector<bool>>(n/2 + 1, vector<bool>(sum + 1, true)));
-        for(int k = 1; k <= n / 2; k++){
-            if(((sum * k) % n) == 0 && possible(A, 0, k, sum * k / n))  return true;
+    vector<vector<vector<bool>>> dp;
+    bool iter(int idx,float sum,float num,float avg,vector<int>& nums){
+        if(idx>=nums.size())
+            return false;
+        
+        if(num>0 and avg==sum/num){
+            
+            return true;
         }
-        return false;
+        
+        if(dp[idx][num][sum]==false) return false;
+        
+        if(iter(idx+1,sum+nums[idx],num+1,avg,nums))
+            return dp[idx][num][sum]=true;
+        
+        if(iter(idx+1,sum,num,avg,nums))
+            return dp[idx][num][sum]=true;
+        
+        return dp[idx][num][sum]=false;
+    }
+    
+    bool splitArraySameAverage(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int n=nums.size();
+        dp.resize(n, vector<vector<bool>>(n, vector<bool>(sum + 1, true)));
+        return iter(0,0,0,(float)sum/n,nums);
     }
 };
