7792e4c2a7516d05200e1f38f59070309875c248
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-17 13:55:50 +0530

    Time: 144 ms (78.51%), Space: 44.7 MB (76.75%) - LeetHub

--- 1224-maximum-equal-frequency/1224-maximum-equal-frequency.cpp
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int maxEqualFreq(vector<int>& nums) {
-        int freqofNum[100001]={0},freqofreq[100001]={0};
-        int ans=0,maxFreq=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            int num=nums[i];
-            freqofNum[num]++;
-            freqofreq[freqofNum[num]]++;
-            
-            maxFreq=max(maxFreq,freqofNum[num]);
-            
-            if( (freqofreq[maxFreq]==1 && maxFreq+(maxFreq-1)*(freqofreq[maxFreq-1]-1) == i+1 ) or 
-                (freqofreq[maxFreq]*maxFreq + 1 == i+1))
-                ans=i+1;
-        }
+int maxEqualFreq(vector<int>& nums) {
+	unordered_map <int , int> freq;
+	unordered_map <int , int> elements_with_freq;
 
-        if(maxFreq==1)
-            return nums.size();
-        return ans;
-    }
+	int ans = 0 , x , n = nums.size();
+
+	for(int i = 0 ; i < n ; i++) {
+		x = nums[i];
+		freq[x]++;
+		elements_with_freq[freq[x]]++;
+
+		//check if all elements have same frequency
+		if(freq[x] * elements_with_freq[freq[x]] == (i + 1)) {
+			if(i + 1 < n)
+				ans = i + 2;
+		}
+		//check if all elements have same frequency except for one extra element
+		if(freq[x] * elements_with_freq[freq[x]] == i) {
+			ans = i + 1;
+		}
+	}
+
+	return ans;
+}
 };
