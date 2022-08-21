026cd1f139105fb4088edb4b10e169720d6b0b5b
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-21 16:48:10 +0530

    Time: 5 ms (96.50%), Space: 6.3 MB (93.19%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,27 +1,19 @@
 class Solution {
     public:
-    int move(int source, int target){
-        if(source == 26) return 0;
-        int y = source / 6;
-        int x = source % 6;
-        int y2 = target / 6;
-        int x2 = target % 6;
-        return abs(y2-y) + abs(x2-x);
-    }
-    
     int minimumDistance(string word) {
-        int dp[word.size()+1][27][27];
-        memset(dp,0,sizeof dp);
-        for(int i=word.size()-1;i>-1;i--){
-            for(int j=0;j<27;j++){
-                for(int k=0;k<27;k++){
-                    int f1move = move(j,word[i]-'A');
-                    int f2move = move(k,word[i]-'A');
-                    dp[i][j][k] = min(dp[i+1][word[i] - 'A'][k] + f1move,dp[i+1][j][word[i] - 'A'] + f2move);
-                }
-            }  
+        vector<int> dp(26);
+        int res = 0, save = 0, n = word.size();
+        for (int i = 0; i < n - 1; ++i) {
+            int b = word[i] - 'A', c = word[i + 1] - 'A';
+            for (int a = 0; a < 26; ++a)
+                dp[b] = max(dp[b], dp[a] + d(b, c) - d(a, c));
+            save = max(save, dp[b]);
+            res += d(b, c);
         }
+        return res - save;
+    }
 
-        return dp[0][26][26];
+    int d(int a, int b) {
+        return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
     }
 };
