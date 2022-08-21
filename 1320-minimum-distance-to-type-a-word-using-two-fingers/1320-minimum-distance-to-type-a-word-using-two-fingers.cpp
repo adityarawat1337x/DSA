d77951911b104bf3fe01752c501ad8e17b24fb53
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-21 16:54:09 +0530

    Time: 11 ms (91.44%), Space: 6.3 MB (93.19%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -2,17 +2,19 @@ class Solution {
     public:
     int minimumDistance(string word) {
         vector<int> dp(26);
-        int res = 0, save = 0, n = word.size();
-        for (int i = 0; i < n - 1; ++i) {
-            int b = word[i] - 'A', c = word[i + 1] - 'A';
-            for (int a = 0; a < 26; ++a)
-                dp[b] = max(dp[b], dp[a] + d(b, c) - d(a, c));
-            save = max(save, dp[b]);
-            res += d(b, c);
+        int ans=0,save=0;
+        
+        for(int i=0;i<word.size()-1;i++){
+            int right=word[i]-'A',next=word[i+1]-'A';
+            for(int left=0;left<26;left++){
+                dp[right]=max(dp[right],dp[left]+d(right,next)-d(left,next));
+            }
+            save = max(save,dp[right]);
+            ans+=d(right,next);
         }
-        return res - save;
+        return ans-save;
     }
-
+                              
     int d(int a, int b) {
         return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
     }
