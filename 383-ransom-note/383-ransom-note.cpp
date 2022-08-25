b8d787c7d785fa975c63c25e0b8bec5bae3f6bdd
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-08-25 10:58:17 +0530

    Time: 55 ms (11.24%), Space: 8.6 MB (89.81%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26,0);
+        for(auto &i:ransomNote)
+            freq[i-'a']++;
+        
+        for(auto &i:magazine)
+            freq[i-'a']--;
+        
+        for(auto &i:freq)
+            if(i>0) return false;
+        return true;
+    }
+};
