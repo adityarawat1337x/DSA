cd6f30da1c64859e3ba9fd21decc2d775272142e
Aditya Rawat <54580337+adityarawat1337x@users.noreply.github.com>
2022-09-03 18:52:38 +0530

    Time: 20 ms (5.31%), Space: 43.1 MB (45.00%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,25 +1,29 @@
 class Solution {
-public:
-    vector<int> ans;
-    int dig;
-    void solve(long x,int k,int n){
-        if(dig==n){
-            ans.push_back(x);
-        }
-        if(dig<n)
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    int lim,diff;
+    void solve(int num,int digits){
+        if(digits==lim){
+            arr.add(num);
             return;
+        }
         
+        if(digits>lim) return;
         
-        int next = (x%10)+k,next2=(x%10)-k;
-        if(next<10 and next>-1)
-            solve(x*10 + next,k,n+1);
-        if(next2<10 and next2>-1 and next!=next2)
-            solve(x*10 + next2,k,n+1);
+        int nex = num%10+diff, nex2 = num%10-diff;
+        
+        if(nex>-1 && nex<10)
+            solve(num*10 + nex,digits+1);
+        
+        if(nex2>-1 && nex2<10 && nex!=nex2)
+            solve(num*10 + nex2,digits+1);
     }
     
-    vector<int> numsSameConsecDiff(int n, int k) {
-        dig=n;
-        for(long i=1;i<10;i++) solve(i,k,1);
-        return ans;
+    public int[] numsSameConsecDiff(int n, int k) {
+        lim = n;
+        diff = k; 
+        for(int i=1;i<10;i++)
+            solve(i,1);
+        for(int a:arr) System.out.print(a);
+        return arr.stream().mapToInt(i -> i).toArray();
     }
-};
+}
